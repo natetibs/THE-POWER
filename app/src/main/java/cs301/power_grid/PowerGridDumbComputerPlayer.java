9ad96b23fef863fdaf1548043f70574b2fec72ee
Luke Douville <luke.douville@gmail.com>
2017-04-21 15:34:21 -0700

    Fixed some broken elements in dumb AI.

--- app/src/main/java/cs301/power_grid/PowerGridDumbComputerPlayer.java
+++ app/src/main/java/cs301/power_grid/PowerGridDumbComputerPlayer.java
@@ -17,6 +17,8 @@ public class PowerGridDumbComputerPlayer extends GameComputerPlayer {
     private GameMainActivity activity;
     PowerState powerState = new PowerState();
     private int selectNum = -1;
+    Inventory inv;
+    private int money;
     /**
      * constructor
      *
@@ -39,22 +41,28 @@ public class PowerGridDumbComputerPlayer extends GameComputerPlayer {
                 /*Use a random integer because this is the dumb AI.*/
                 int randOKPass = (int)(Math.random() * 2);
                 int randPPlantToBidOnIndex = (int)(Math.random() * 4);
+                selectNum = randPPlantToBidOnIndex;
                 if(randOKPass == 0) {
-                    PassAction pa = new PassAction(PowerGridDumbComputerPlayer.this);
-                    game.sendAction(pa);
-                }
-                else {
                     SelectPowerPlantAction sppa = new SelectPowerPlantAction(PowerGridDumbComputerPlayer.this, selectNum);
                     game.sendAction(sppa);
+                }
+                else {
+                    PassAction pa = new PassAction(PowerGridDumbComputerPlayer.this);
+                    game.sendAction(pa);
                 }
 
             /*Make sure the dumb AI has enough money to buy anything.*/
             } else if(phase == 1) {
                 /*Bidding on power plant(s).
                 * Bidding or "pass" updates phase.*/
+                money = inv.getMoney();
+                if(money < powerState.getAvailPowerplant().get(0).getCost()) {
+                    /*Don't bid*/
+                    PassAction pa = new PassAction(PowerGridDumbComputerPlayer.this);
+                    game.sendAction(pa);
+                }
                 BidAction bid;
                 int randOKPass = (int)(Math.random()*2);
-                /*You could just put this in an else statement after evaluating randOKPass below.*/
                 if(randOKPass == 0) {
                     bid = new BidAction(this, (int)Math.random()*20);
                     bid.getBid();
@@ -72,8 +80,15 @@ public class PowerGridDumbComputerPlayer extends GameComputerPlayer {
                 * "OK" or "Pass" updates phase.*
                 * Same as when phase is 0 b/c same logic.
                 * We don't determine who the previous passer was in this if statement.*/
+                money = inv.getMoney();
+                if(money < powerState.getAvailPowerplant().get(0).getCost()) {
+                    /*Don't bid*/
+                    PassAction pa = new PassAction(PowerGridDumbComputerPlayer.this);
+                    game.sendAction(pa);
+                }
                 int randOKPass = (int)(Math.random() * 2);
-                int randPPlantToBidOn = (int)(Math.random() * 42);
+                int randPPlantToBidOn = (int)(Math.random() * 4);
+                selectNum = randPPlantToBidOn;
                 /*You could just put this in an else statement after evaluating randOKPass below.*/
                 PassAction pa = new PassAction(PowerGridDumbComputerPlayer.this);
                 game.sendAction(pa);
