03a5af358fadaadd5a6d00c60ebe897fc68db806
natetibs <natetibs@gmail.com>
2017-04-29 17:22:12 -0700

    AI incoming

--- app/src/main/java/cs301/power_grid/PowerGridSmartComputerPlayer.java
+++ app/src/main/java/cs301/power_grid/PowerGridSmartComputerPlayer.java
@@ -1,5 +1,8 @@
 package cs301.power_grid;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import game.GameComputerPlayer;
 import game.infoMsg.GameInfo;
 
@@ -13,6 +16,10 @@ public class PowerGridSmartComputerPlayer extends GameComputerPlayer{
     private int selectNum = -1;
     Inventory inv;
     private int money;
+    private boolean first = true;
+    private boolean bananas = true;
+    ArrayList<BuyCityAction> cityActionList = new ArrayList<BuyCityAction>();
+    ArrayList<BuyResourceAction> actionArray = new ArrayList<BuyResourceAction>();
     /**
      * constructor
      *
@@ -34,40 +41,162 @@ public class PowerGridSmartComputerPlayer extends GameComputerPlayer{
             //get computer inventory
             inv = powerState.getGameInventories().get(1);
             /*Make sure the dumb AI has enough money to buy anything.*/
+
+            int tempPowered = -1;
+            int index = 0;
+            for(int i = 0; i< 4; i++){
+                if(powerState.getAvailPowerplant().get(i).getHp() > tempPowered){
+                    tempPowered = powerState.getAvailPowerplant().get(i).getHp();
+                    index = i;
+                }
+            }
+
+            selectNum = index;
+
+
             if (phase == 0) {
 
-                PassAction pa = new PassAction(PowerGridSmartComputerPlayer.this);
-                game.sendAction(pa);
+                SelectPowerPlantAction sppa = new SelectPowerPlantAction(PowerGridSmartComputerPlayer.this, selectNum);
+                game.sendAction(sppa);
 
             } else if(phase == 1) {
 
-                PassAction pa = new PassAction(PowerGridSmartComputerPlayer.this);
-                game.sendAction(pa);
+                if(Math.random() > .5) {
+                    int prevBid = powerState.getCurrentBid();
+                    BidAction ba = new BidAction(PowerGridSmartComputerPlayer.this, prevBid + 1);
+                    game.sendAction(ba);
+                }
+                else {
+
+                    game.sendAction(new PassAction(PowerGridSmartComputerPlayer.this));
+                }
 
             } else if(phase == 2) {
 
-                PassAction pa = new PassAction(PowerGridSmartComputerPlayer.this);
-                game.sendAction(pa);
+                SelectPowerPlantAction sppa = new SelectPowerPlantAction(PowerGridSmartComputerPlayer.this, selectNum);
+                game.sendAction(sppa);
 
             } else if(phase == 3) {
 
-                PassAction pa = new PassAction(PowerGridSmartComputerPlayer.this);
-                game.sendAction(pa);
+
+                if(first) {
+                    for (int i = 0; i < inv.getMyPlants().size(); i++) {
+                        if (inv.getMyPlants().get(i).getKind().equals("Oil")) {
+                            for (int j = 0; j < inv.getMyPlants().get(i).getPtP(); j++) {
+                                actionArray.add(new BuyResourceAction(PowerGridSmartComputerPlayer.this, j, "oil"));
+                            }
+                        }
+                        if (inv.getMyPlants().get(i).getKind().equals("Coal")) {
+                            for (int j = 0; j < inv.getMyPlants().get(i).getPtP(); j++) {
+                                actionArray.add(new BuyResourceAction(PowerGridSmartComputerPlayer.this, j, "coal"));
+                            }
+                        }
+                        if (inv.getMyPlants().get(i).getKind().equals("Uranium")) {
+                            for (int j = 0; j < inv.getMyPlants().get(i).getPtP(); j++) {
+                                actionArray.add(new BuyResourceAction(PowerGridSmartComputerPlayer.this, j, "uranium"));
+                            }
+                        }
+                        if (inv.getMyPlants().get(i).getKind().equals("Trash")) {
+                            for (int j = 0; j < inv.getMyPlants().get(i).getPtP(); j++) {
+                                actionArray.add(new BuyResourceAction(PowerGridSmartComputerPlayer.this, j, "trash"));
+                            }
+                        }
+                    }
+                    first = false;
+                }
+
+                if(actionArray.size() == 0){
+                    first = true;
+                    game.sendAction(new PassAction(PowerGridSmartComputerPlayer.this));
+                }
+                else{
+
+                    game.sendAction(actionArray.remove(0));
+                }
 
             } else if(phase == 4) {
 
-                PassAction pa = new PassAction(PowerGridSmartComputerPlayer.this);
-                game.sendAction(pa);
+
+                if(first) {
+                    for (int i = 0; i < inv.getMyPlants().size(); i++) {
+                        if (inv.getMyPlants().get(i).getKind().equals("Oil")) {
+                            for (int j = 0; j < inv.getMyPlants().get(i).getPtP(); j++) {
+                                actionArray.add(new BuyResourceAction(PowerGridSmartComputerPlayer.this, j, "oil"));
+                            }
+                        }
+                        if (inv.getMyPlants().get(i).getKind().equals("Coal")) {
+                            for (int j = 0; j < inv.getMyPlants().get(i).getPtP(); j++) {
+                                actionArray.add(new BuyResourceAction(PowerGridSmartComputerPlayer.this, j, "coal"));
+                            }
+                        }
+                        if (inv.getMyPlants().get(i).getKind().equals("Uranium")) {
+                            for (int j = 0; j < inv.getMyPlants().get(i).getPtP(); j++) {
+                                actionArray.add(new BuyResourceAction(PowerGridSmartComputerPlayer.this, j, "uranium"));
+                            }
+                        }
+                        if (inv.getMyPlants().get(i).getKind().equals("Trash")) {
+                            for (int j = 0; j < inv.getMyPlants().get(i).getPtP(); j++) {
+                                actionArray.add(new BuyResourceAction(PowerGridSmartComputerPlayer.this, j, "trash"));
+                            }
+                        }
+                    }
+                    first = false;
+                }
+
+                if(actionArray.size() == 0){
+                    first = true;
+                    game.sendAction(new PassAction(PowerGridSmartComputerPlayer.this));
+                }
+                else{
+
+                    game.sendAction(actionArray.remove(0));
+                }
 
             } else if(phase == 5) {
 
-                PassAction pa = new PassAction(PowerGridSmartComputerPlayer.this);
-                game.sendAction(pa);
+
+                int tempMoney = inv.getMoney();
+                if(bananas) {
+                    for (int i = 0; i < 20; i++) {
+                        if (tempMoney > 10) {
+                            cityActionList.add(new BuyCityAction(PowerGridSmartComputerPlayer.this, powerState.getAvailCities().get(i), i));
+                            tempMoney -= 10;
+                        }
+                    }
+                    bananas = false;
+                }
+
+                if(cityActionList.size() == 0){
+                    bananas = true;
+                    game.sendAction(new PassAction((PowerGridSmartComputerPlayer.this)));
+                }
+                else{
+                    game.sendAction(cityActionList.remove(0));
+                }
+
+
             /*Pass*/
             } else if(phase == 6) {
 
-                PassAction pa = new PassAction(PowerGridSmartComputerPlayer.this);
-                game.sendAction(pa);
+
+                int tempMoney = inv.getMoney();
+                if(bananas) {
+                    for (int i = 0; i < 20; i++) {
+                        if (tempMoney > 10) {
+                            cityActionList.add(new BuyCityAction(PowerGridSmartComputerPlayer.this, powerState.getAvailCities().get(i), i));
+                            tempMoney -= 10;
+                        }
+                    }
+                    bananas = false;
+                }
+
+                if(cityActionList.size() == 0){
+                    bananas = true;
+                    game.sendAction(new PassAction((PowerGridSmartComputerPlayer.this)));
+                }
+                else{
+                    game.sendAction(cityActionList.remove(0));
+                }
             }
         }
 
